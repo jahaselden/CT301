bc8eed213062264227f1eaeb676b3f72b11b70a1
jenny haselden <jhaselde@colostate.edu>
2024-09-26 10:35:31 -0600

    incorrect number of rows printing, first submission

--- ASN1/game_of_life.cpp
+++ ASN1/game_of_life.cpp
@@ -7,43 +7,237 @@
 #include <string>
 
 using namespace std;
-//@author Your name goes here
-//TODO: Include the Namespace for Game of Life
+//@author Jennifer Haselden
+// Include the Namespace for Game of Life
+namespace GOL
+{
+    game_of_life::game_of_life(string filename)
+    {
+        // Create a file input using the user's input
+        fstream file_in(filename);
+        if (!file_in)
+        {
+            // File not found
+            throw(runtime_error("File not found as entered: please make sure file path is correct"));
+        }
+        if (!(file_in >> this->width_))
+        {
+            // Invalid file format
+            throw(runtime_error("Invalid file format: first line must be width as an int, and the following lines must be the table"));
+        }
+        cout << "WIDTH:" << width_ << endl;
+        // Pre Load "Current" with the "dead_cell" character as a square table
+        // Remember that the default dead character is '-'
+        // is this right..?
+        this->height_ = width_;
+        size_t size = width_ * height_;
+        this->current_ = string(size, '-'); /*look up string fill ctor*/
 
-game_of_life::game_of_life(string filename) {
-  // Create a file input using the user's input
-  fstream file_in(filename);
-  if (!file_in) {
-    // File not found
-    throw(runtime_error(/*FINISH THIS ERROR MESSAGE*/));
-  }
-  if (!(file_in >> this->width_)) {
-    // Invalid file format
-    throw(runtime_error(/*FINISH THIS ERROR MESSAGE*/))
-  }
-  // Pre Load "Current" with the "dead_cell" character as a square table
-  this->current_ = "";/*look up string fill ctor*/
-  // Remember that the default dead character is '-'
-  string line;
-  getline(file_in, line);
-  // Skipping the end of the first line to get to the data
-  // If you do not include this, then the first "getline"
-  // will get an empty string
-  // For all rows in the file
-  for (int row = 0; row < this->width_; ++row) {
-    getline(file_in, line);
-    // For all Columns in the row
-    for (int col = 0; col < this->width_; ++col) {
-      // TODO: When you find a "living" character inside of "line"
-      // Set the character at the same index inside of current to be "live"
-      // Remember that the default live character is '*'
-      // Check the assignment page for the math logic for 2d to 1d conversion
+        string line;
+        getline(file_in, line);
+        // Skipping the end of the first line to get to the data
+        // If you do not include this, then the first "getline"
+        // will get an empty string
+        // For all rows in the file
+        for (int row = 0; row < this->width_; ++row)
+        {
+            getline(file_in, line);
+            // For all Columns in the row
+            for (int col = 0; col < this->height_; ++col)
+            {
+                // TODO: When you find a "living" character inside of "line"
+                // Set the character at the same index inside of current to be "live"
+                // Remember that the default live character is '*'
+                // Check the assignment page for the math logic for 2d to 1d conversion
+
+                //if the char is alive in the line, set it to be alive in current game
+                if (line.at(col) == '*'){
+                    int replace_index = (row * width_) + col;
+                    current_[replace_index] = '*';
+                }
+            }
+            // should i add a newline here?
+        }
     }
-  }
-}
-//TODO Implement the Game of Life Functions
 
-//TODO Finish the Operator<< method for outputting your game of life
-ostream &operator<<(ostream &os, const game_of_life &game) {
-  return os;
+    // TODO Implement the Game of Life Functions
+    int game_of_life::Convert2DTo1D(int row, int col)
+    {
+        return (row * width_) + col;
+    }
+
+    int game_of_life::Get2DRow(int index)
+    {
+        return index / width_; // does / automatically floor if int type?
+    }
+
+    int game_of_life::Get2DCol(int index)
+    {
+        return index % height_;
+    }
+
+    void game_of_life::NextGen()
+    {
+        std::string next_gen = current_; // initialize next gen string to current game
+
+        // for all cells in the current_ game
+        for (size_t i = 0; i < current_.length(); i++)
+        {
+            // check current cell's state
+            bool alive = CheckCellState(i);
+
+            // check the current cell's neighbor's states
+            int living_neighbors = CheckLivingNeighbors(i);
+
+            // based on current cell state and neighbors of current cell, change
+            // cell state for next gen
+            bool alive_in_next_gen = DetermineCellStateForNextGen(living_neighbors, alive);
+            SetCellState(alive_in_next_gen, i, next_gen);
+        }
+        // when all cells have been accounted for - move on to next round 
+        CompleteNextGen(next_gen);
+    }
+
+    int game_of_life::CheckLivingNeighbors(int cell_index) // might need to be size_t
+    {
+        int middle_row = Get2DRow(cell_index);
+        int middle_col = Get2DCol(cell_index);
+        int left_col = GetLeftCol(middle_col);
+        int right_col = GetRightCol(middle_col);
+        int upper_row = GetUpperRow(middle_row);
+        int lower_row = GetLowerRow(middle_row);
+
+        // int up_left = Convert2DTo1D(upper_row, left_col);
+        // int middle_left = Convert2DTo1D(middle_row, left_col);
+        // int lower_left = Convert2DTo1D(lower_row, left_col);
+        // int up_middle = Convert2DTo1D(upper_row, middle_col);
+        // int down_middle = Convert2DTo1D(lower_row, middle_col);
+        // int up_right = Convert2DTo1D(upper_row, right_col);
+        // int middle_right = Convert2DTo1D(middle_row, right_col);
+        // int lower_right = Convert2DTo1D(lower_row, right_col);
+
+        // try array loop instead of a bunch of if else statements for comparison
+        int neighbors[8] = {Convert2DTo1D(upper_row, left_col),
+                            Convert2DTo1D(middle_row, left_col),
+                            Convert2DTo1D(lower_row, left_col),
+                            Convert2DTo1D(upper_row, middle_col),
+                            Convert2DTo1D(lower_row, middle_col),
+                            Convert2DTo1D(upper_row, right_col),
+                            Convert2DTo1D(middle_row, right_col),
+                            Convert2DTo1D(lower_row, right_col)};
+
+        int living_neighbors = 0;
+
+        // increment living_neighbors if char is * of cell
+        for (int neighbor_index : neighbors)
+        {
+            if (current_.at(neighbor_index) == '*')
+            {
+                living_neighbors++;
+            }
+        }
+
+        return living_neighbors;
+    }
+
+    bool game_of_life::DetermineCellStateForNextGen(int living_neighbors, bool alive)
+    {
+        bool alive_in_next_gen = false;
+        if (alive == true)
+        {
+            // A live cell with two or three live neighbors lives on to the next generation
+            if (living_neighbors == 2 || living_neighbors == 3)
+            {
+                alive_in_next_gen = true;
+            }
+            // A live cell with fewer than two live neighbors dies (underpopulation)
+            // A live cell with more than three live neighbors dies (overpopulation)
+            else
+            {
+                alive_in_next_gen = false;
+            }
+        }
+        // A dead cell with exactly three live neighbors comes to life (rebirth)
+        else if (alive == false && living_neighbors == 3)
+        {
+            alive_in_next_gen = true;
+        }
+        return alive_in_next_gen;
+    }
+
+    void game_of_life::SetCellState(bool alive, size_t cell_index, string &next_gen_game)
+    {
+        if (alive)
+        {
+            next_gen_game[cell_index] = '*';
+        }
+        else
+        {
+            next_gen_game[cell_index] = '-';
+        }
+    }
+
+    void game_of_life::CompleteNextGen(string &next_gen_game){
+        current_ = next_gen_game;
+    }
+
+    bool game_of_life::CheckCellState(size_t cell_index)
+    {
+        if (current_[cell_index] == '*')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void game_of_life::NextNGen(int n)
+    {
+        if (n > 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                NextGen();
+            }
+        }
+    }
+
+    int game_of_life::GetLeftCol(int col)
+    {
+        return ((col - 1) + width_) % width_;
+    }
+
+    int game_of_life::GetRightCol(int col)
+    {
+        return (col + 1) % width_;
+    }
+
+    int game_of_life::GetUpperRow(int row)
+    {
+        return ((row - 1) + height_) % height_;
+    }
+
+    int game_of_life::GetLowerRow(int row)
+    {
+        return (row + 1) % height_;
+    }
+
+    int game_of_life::GetGenerations()
+    {
+        return generations_;
+    }
+
+    // TODO Finish the Operator<< method for outputting your game of life
+    ostream &operator<<(ostream &os, const game_of_life &game)
+    {
+        for (size_t i = 0; i < game.current_.length() + 1; i++){
+            os << game.current_[i];
+            if ((i + 1) % game.width_ == 0){
+                os << endl;
+            }
+        }
+        return os;
+    }
 }
--- ASN1/game_of_life.h
+++ ASN1/game_of_life.h
@@ -2,31 +2,145 @@
 #define GAME_OF_LIFE_H_DEFINED
 #include <iostream>
 #include <string>
-//@author Your name goes here
-namespace /*insert correct namespace name*/ {
-class game_of_life {
-  // Insert Data Member Variables here
-  /***
-   * int width_, this integer stores the value for the width and height
-   * of the game table
-   * TODO: INCLUDE THE OTHER 2 CLASS MEMBER VARIABLES
-   */
-  int width_;
-  int generations_ = 0;
-  std::string current_;
+//@author Jennifer Haselden
+namespace GOL
+{
+  class game_of_life
+  {
+    // Insert Data Member Variables here
+    /***
+     * int width_, this integer stores the value for the width and height
+     * of the game table
+     * TODO: INCLUDE THE OTHER 2 CLASS MEMBER VARIABLES
+     */
+    int width_;
+    int height_; // use to make asgn2 easier?
+    int generations_ = 0;
+    std::string current_;
 
- public:
-  // No need to include a doxygen comment for a deleted ctor
-  game_of_life() = delete;
-  // TODO Write Doxygen comment for this constructor,
-  // Look at the implementation in game_of_life.cpp for reference
-  game_of_life(std::string filename);
-  //TODO Define the Remaining Methods of Game of Life here
-  //NOTE: Nothing needed for a friend statement
-  friend std::ostream &operator<<(std::ostream &os, const game_of_life &game);
-};
-//TODO Write a Doxygen comment for your output operator function
-std::ostream &operator<<(std::ostream &os, const game_of_life &game);
+  public:
+    game_of_life() = delete;
+
+    /**
+     * game_of_life
+     * this constructor loads a game of life with the user's input file
+     * @param filename is a file that represents the "current" game of life
+     */
+    game_of_life(std::string filename);
+    // TODO Define the Remaining Methods of Game of Life here
+    // and their doxygen
+
+    /**
+     * Convert2DTo1D
+     * @param row is the row number of 2D structure
+     * @param col is the col number of 2D structure
+     * @returns an int to index into 1D format string
+     */
+    int Convert2DTo1D(int row, int col);
+
+    /**
+     * Get2DRow
+     * @param index is an index in 1D format to be converted to row
+     * @returns an int that represents the row of passed index
+     */
+    int Get2DRow(int index);
+
+    /**
+     * Get2DCol
+     * @param index is an index in 1D format to be converted to col
+     * @returns an int that represents the col of passed index
+     */
+    int Get2DCol(int index);
+
+    /**
+     * NextGen
+     * Calculates the next generation of the game and increments generation count
+     */
+    void NextGen();
+
+    /**
+     * CheckLivingNeighbors
+     * @param cell_index is the cell to check surrounding neighbors in current_ game
+     * @returns an int with number of living neighbors of passed cell
+     */
+    int CheckLivingNeighbors(int cell_index);
+
+    /**
+     * GetLeftCol
+     * @param col the current column of the cell
+     * @returns the col to the left of the cell
+     */
+    int GetLeftCol(int col);
+
+    /**
+     * GetRightCol
+     * @param col the current column of the cell
+     * @returns the col to the right of the cell
+     */
+    int GetRightCol(int col);
+
+    /**
+     * GetUpperRow
+     * @param row the current row of the cell
+     * @returns the row above the current cell
+     */
+    int GetUpperRow(int row);
+
+    /**
+     * GetLowerRow
+     * @param row the current row of the cell
+     * @returns the row below the current cell
+     */
+    int GetLowerRow(int row);
+
+    /**
+     * DetermineCellStateForNextGen
+     * @param living_neighbors an int representing number of live neighbors
+     * @returns a boolean that represents if a cell will be alive (true), or dead (false)
+     * in the next gen based on the rules of Game of Life & number of alive neighbors
+     */
+    bool DetermineCellStateForNextGen(int living_neighbors, bool alive);
+
+    /**
+     * CheckCellState
+     * @param cell_index to check state
+     * @returns boolean representing current state of alive or dead (true or false)
+     */
+    bool CheckCellState(size_t cell_index);
+
+    /**
+     * SetCellState
+     * @param alive_in_next_gen is the bool representing if a cell will be alive (true), or dead 
+     * (false) in next round
+     * @param cell_index the cell that needs dead or alive state to be set
+     * @param next_gen_game a string that represents what will be set to the next gen
+     * after round completes
+     */
+    void SetCellState(bool alive, size_t cell_index, std::string &next_gen_game);
+
+    /**
+     * CompleteNextGen
+     * @param next_gen_game is set as the current_ game once all cells have been updated
+     */
+    void CompleteNextGen(std::string &next_gen_game);
+
+    /**
+     * NextNGen
+     * @param n the number of times to run the NextGen() method, ignores n < 1
+     */
+    void NextNGen(int n);
+
+    /**
+     * GetGenerations
+     * @returns the number of generations
+     */
+    int GetGenerations();
+
+    // NOTE: Nothing needed for a friend statement
+    friend std::ostream &operator<<(std::ostream &os, const game_of_life &game);
+  };
+  // TODO Write a Doxygen comment for your output operator function
+  std::ostream &operator<<(std::ostream &os, const game_of_life &game);
 }
 
 #endif
