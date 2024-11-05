f588ccd3f755e1808829394822f42ff1120c7414
jenny haselden <jhaselde@colostate.edu>
2024-11-05 12:46:23 -0700

    update copy ctro, = operator, and operator declarations

--- HW3/double_vector.cpp
+++ HW3/double_vector.cpp
@@ -1,156 +1,173 @@
 #include "double_vector.h"
 #include <cstddef>
+#include <stddef.h>
 
-using namespace double_vector;
 using namespace std;
 
-namespace double_vector
+double_vector::double_vector() : double_vector(0) {}
+double_vector::double_vector(size_t capacity) : size_(0)
 {
-    double_vector::double_vector() : double_vector(0) {}
-    double_vector::double_vector(size_t capacity) : size_(0)
+    if (capacity > max_size())
     {
-        if (capacity > max_size())
-        {
-            capacity_ = max_size();
-        }
-        else
-        {
-            capacity_ = capacity;
-        }
-        data = new double[capacity_];
+        capacity_ = max_size();
     }
-    // TODO - implement
-    double_vector::double_vector(const double_vector &rhs)
+    else
     {
-        this->capacity_ = rhs.capacity();
-        this->size_ = rhs.size();
-        this->data = new double[capacity_];
-        for (size_t i = 0; i < capacity_; i++)
-        {
-            data[i] = rhs.data[i];
-        }
+        capacity_ = capacity;
     }
-    double_vector::double_vector &operator=(const double_vector &rhs)
+    this->data_ = new double[this->capacity_];
+}
+
+double_vector::double_vector(const double_vector &rhs)
+{
+    // this->capacity_ = rhs.capacity_;
+    // this->size_ = rhs.size_;
+    // this->data_ = new double[capacity_];
+    // for (size_t i = 0; i < capacity_; i++)
+    // {
+    //     data_[i] = rhs.data_[i];
+    // }
+    *this = double_vector(rhs);
+}
+
+double_vector &double_vector::operator=(const double_vector &rhs)
+{
+    this->capacity_ = rhs.capacity_;
+    this->size_ = rhs.size_;
+    this->data_ = new double[capacity_];
+    for (size_t i = 0; i < capacity_; i++)
     {
-        // use iterator instead for this?
-        //  capacity_ = rhs.capacity();
-        //  this->size_ = rhs.size();
-        //  // this->data = new double[capacity_];
-        //  for (size_t i = 0; i < capacity_; i++)
-        //  {
-        //      data[i] = rhs.data[i];
-        //  }
+        data_[i] = rhs.data_[i];
     }
+}
 
-    double_vector::~double_vector()
+double_vector::~double_vector()
+{
+    delete[] data_;
+    data_ = nullptr;
+}
+
+// CAPACITY METHODS
+
+size_t double_vector::size()
+{
+    return size_;
+}
+
+size_t double_vector::max_size()
+{
+    return ((2 ^ 32) / sizeof(double)) - 1;
+}
+
+size_t double_vector::capacity()
+{
+    return capacity_;
+}
+
+void double_vector::resize(size_t n, double val)
+{
+}
+
+bool double_vector::empty()
+{
+    return size_ == 0 ? true : false;
+}
+
+void double_vector::reserve(size_t n)
+{
+}
+
+void double_vector::shrink_to_fit()
+{
+}
+
+// ELEMENT ACCESS
+
+double double_vector::&operator[](size_t n)
+{
+}
+
+double double_vector::&at(size_t n)
+{
+}
+
+double double_vector::&front()
+{
+}
+
+double double_vector::&back()
+{
+}
+
+double double_vector::*data()
+{
+}
+
+double_vector::iterator begin()
+{
+}
+
+double_vector::iterator end()
+{
+}
+
+// MODIFIERS
+void double_vector::reallocate(size_t n)
+{
+    double *new_data = new double[n];
+    // if n is smaller than old data's size,
+    // only copy n values into new array
+    for (int i = 0; i < n && i < size_; i++)
     {
-        delete[] data;
-        data = nullptr;
+        // to do
     }
+    delete[] data_;
+    data_ = new_data; // is this right?
+}
 
-    // CAPACITY METHODS
+void double_vector::push_back(const double &val)
+{
+}
 
-    size_t double_vector::size()
-    {
-        return size_;
-    }
+void double_vector::pop_back()
+{
+}
 
-    size_t double_vector::max_size()
-    {
-        return ((2 ^ 32) / sizeof(double)) - 1;
-    }
+void double_vector::swap(double_vector &other)
+{
+}
 
-    size_t double_vector::capacity()
-    {
-        return capacity_;
-    }
+void double_vector::clear()
+{
+    return size_ == 0 ? true : false;
+}
 
-    void double_vector::resize(size_t n, double val)
-    {
-    }
+// ITERATOR SUBCLASS
 
-    bool double_vector::empty()
-    {
-        return size_ == 0 ? true : false;
-    }
+double_vector::iterator &double_vector::iterator::operator++()
+{
+}
 
-    void double_vector::reserve(size_t n)
-    {
-    }
+double_vector::iterator double_vector::iterator::operator++(int)
+{
+}
 
-    void double_vector::shrink_to_fit()
-    {
-    }
+double_vector::iterator &double_vector::iterator::operator--()
+{
+}
 
-    // ELEMENT ACCESS
+double_vector::iterator double_vector::iterator::operator--(int)
+{
+}
 
-    double double_vector::&operator[](size_t n)
-    {
-    }
+double &double_vector::iterator::operator*()
+{
+}
 
-    double double_vector::&at(size_t n)
-    {
-    }
+bool double_vector::iterator::operator==(const iterator &) const
+{
+}
 
-    double double_vector::&front()
-    {
-    }
-
-    double double_vector::&back()
-    {
-    }
-
-    double double_vector::*data()
-    {
-    }
-
-    iterator begin();
-
-    iterator end();
-
-    // MODIFIERS
-    void double_vector::reallocate(size_t n)
-    {
-        double *new_data = new double[n];
-        // if n is smaller than old data's size, 
-        // only copy n values into new array
-        for (int i = 0; i < n && i < size_; i++){
-            //to do 
-        }
-        delete[] data;
-        data = &new_data; //is this right?
-
-    }
-
-    void double_vector::push_back(const double &val)
-    {
-    }
-
-    void double_vector::pop_back()
-    {
-    }
-
-    void double_vector::swap(double_vector &other)
-    {
-    }
-
-    void double_vector::clear()
-    {
-    }
-
-    // ITERATOR SUBCLASS
-
-    iterator double_vector::iterator &operator++();
-
-    iterator double_vector::iterator operator++(int);
-
-    iterator double_vector::iterator &operator--();
-
-    iterator double_vector::iterator operator--(int);
-
-    double double_vector::iterator &operator*();
-
-    bool double_vector::iterator operator==(const iterator &) const;
-
-    bool double_vector::iterator operator!=(const iterator &) const;
+bool double_vector::iterator::operator!=(const iterator &) const
+{
 }
--- HW3/double_vector.h
+++ HW3/double_vector.h
@@ -1,238 +1,242 @@
-#ifndef DOUBLE_VECTOR_INCLUDED
-#define DOUBLE_VECTOR_INCLUDED
+#ifndef DOUBLE_VECTOR_H_INCLUDED
+#define DOUBLE_VECTOR_H_INCLUDED
 
-namespace double_vector
+#include <cmath>
+#include <cstddef>
+#include <iostream>
+#include <stdexcept>
+
+class double_vector
 {
-    class double_vector
+    /**
+     * method name
+     * @param name and description
+     * @return value/type and description
+     */
+
+    /**
+     * Pointer to data array. Remember to use new and delete keywords to properly allocate space for data.
+     */
+    double *data_ = nullptr;
+
+    /**
+     * How much data is currently stored.
+     */
+    std::size_t size_;
+
+    /**
+     * How much data can be stored before reallocation of data. Will double when a reallocation is triggered
+     * due to capacity being full.
+     */
+    std::size_t capacity_;
+
+    /**
+     * reallocate
+     * @param n the new length of data to reallocate to
+     */
+    void reallocate(size_t n);
+
+public:
+    /**
+     * Default constructor defaults to capacity of 0.
+     */
+    double_vector();
+
+    /**
+     * Set the capacity and size to this value and default initialize all values between.
+     */
+    double_vector(size_t capacity);
+
+    /**
+     * Copy constructor (not defaulted). Copies all elements from rhs to lhs.
+     */
+    double_vector(const double_vector &rhs);
+
+    /**
+     * Copy assignment operator. Copies all elements from rhs to lhs.
+     */
+    double_vector &operator=(const double_vector &rhs);
+
+    /**
+     * Cleans up all memory allocations. Clears memory and deletes pointer when done.
+     */
+    ~double_vector();
+
+    // do we default move ctor/assignment or do we just not include them?
+
+    // CAPACITY METHODS
+
+    /**
+     * size
+     * @returns the size of the stored data
+     */
+    size_t size();
+
+    /**
+     * max_size
+     * @returns the value of the largest amount of data that can be stored
+     */
+    size_t max_size();
+
+    /**
+     * capacity
+     * @returns the current capacity before reallocation will occur
+     */
+    size_t capacity();
+
+    /**
+     * resize
+     * @param n the vector length to resize to
+     */
+    void resize(size_t n);
+
+    /**
+     * resize
+     * @param n the vector length to resize to
+     * @param val the default initialization for any empty elements (instead of 0)
+     */
+    void resize(size_t n, double val);
+
+    /**
+     * empty
+     * @returns true if there is no data currently stored in vector (size is 0), false otherwise
+     */
+    bool empty();
+
+    /**
+     * reserve
+     * @param n the size to reallocate to if n is greater than current capacity
+     */
+    void reserve(size_t n);
+
+    /**
+     * shrink_to_fit
+     * @brief reduces the capacity of vector to current size
+     */
+    void shrink_to_fit();
+
+    // ELEMENT ACCESS
+    /**
+     * [] operator overload
+     * @param n the index of value to be accessed
+     * @return the value stored at index n
+     */
+    double &operator[](size_t n);
+
+    /**
+     * at
+     * @param n the index of the value to be accessed
+     * @throws range_errorwhen n is out of bounds
+     */
+    double &at(size_t n);
+
+    /**
+     * front
+     * @returns reference to the first element in the vector
+     */
+    double &front();
+
+    /**
+     * back
+     * @returns reference to the last element in the vector
+     */
+    double &back();
+
+    /**
+     * data
+     * @returns pointer to the data array
+     */
+    double *data();
+
+    // MODIFIERS
+    /**
+     * push_back
+     * @param val the element to be added to the end of the vector
+     */
+    void push_back(const double &val);
+
+    /**
+     * pop_back
+     * @brief removes the last element in the vector reducing size by one
+     */
+    void pop_back();
+
+    /**
+     * swap
+     * @param other the contents to swap data vector with
+     */
+    void swap(double_vector &other);
+
+    /**
+     * clear
+     * @brief empties the array of all values and sets the size to 0
+     */
+    void clear();
+
+    // ITERATOR SUBCLASS
+    class iterator
     {
-        /**
-         * method name
-         * @param name and description
-         * @return value/type and description
-         */
-
-        /**
-         * Pointer to data array. Remember to use new and delete keywords to properly allocate space for data.
-         */
-        double *data;
-
-        /**
-         * How much data is currently stored.
-         */
-        size_t size_;
-
-        /**
-         * How much data can be stored before reallocation of data. Will double when a reallocation is triggered
-         * due to capacity being full.
-         */
-        size_t capacity_;
-
-        /**
-         * reallocate
-         * @param n the new length of data to reallocate to
-         */
-        void reallocate(size_t n);
+        double *ptr;
 
     public:
         /**
-         * Default constructor defaults to capacity of 0.
+         * pre-increment operator
+         * @brief moves the iterator forward and returns
          */
-        double_vector();
+        iterator &operator++();
 
         /**
-         * Set the capacity and size to this value and default initialize all values between.
+         * post-increment operator
+         * @brief moves the iterator forward one step but returns iterator before moving forward
          */
-        double_vector(size_t capacity);
+        iterator operator++(int);
 
         /**
-         * Copy constructor (not defaulted). Copies all elements from rhs to lhs.
+         * pre-decrement operator
+         * @brief moves the iterator backwards one step and returns
          */
-        double_vector(const double_vector &rhs);
+        iterator &operator--();
 
         /**
-         * Copy assignment operator. Copies all elements from rhs to lhs.
+         * post-decrement operator
+         * @brief moves the iterator backwards one step but returns iterator before moving backward
          */
-        double_vector &operator=(const double_vector &rhs);
+        iterator operator--(int);
 
         /**
-         * Cleans up all memory allocations. Clears memory and deletes pointer when done.
+         * de-reference operator
+         * @returns the location of the data the iterator is looking at
          */
-        ~double_vector();
-
-        // do we default move ctor/assignment or do we just not include them?
-
-        // CAPACITY METHODS
+        double &operator*();
 
         /**
-         * size
-         * @returns the size of the stored data
+         * equality operator
+         * @param iterator to compare equality (same position in vector, NOT value)
+         * @returns true if they are at same position in vector, false otherwise
          */
-        size_t size();
+        bool operator==(const iterator &) const;
 
         /**
-         * max_size
-         * @returns the value of the largest amount of data that can be stored
+         * in-equality operator
+         * @param to compare in-equality (not same position in vector)
+         * @returns trues if they are not in the same index or memory location
          */
-        size_t max_size();
-
-        /**
-         * capacity
-         * @returns the current capacity before reallocation will occur
-         */
-        size_t capacity();
-
-        /**
-         * resize
-         * @param n the vector length to resize to
-         */
-        void resize(size_t n);
-
-        /**
-         * resize
-         * @param n the vector length to resize to
-         * @param val the default initialization for any empty elements (instead of 0)
-         */
-        void resize(size_t n, double val);
-
-        /**
-         * empty
-         * @returns true if there is no data currently stored in vector (size is 0), false otherwise
-         */
-        bool empty();
-
-        /**
-         * reserve
-         * @param n the size to reallocate to if n is greater than current capacity
-         */
-        void reserve(size_t n);
-
-        /**
-         * shrink_to_fit
-         * @brief reduces the capacity of vector to current size
-         */
-        void shrink_to_fit();
-
-        // ELEMENT ACCESS
-        /**
-         * [] operator overload
-         * @param n the index of value to be accessed 
-         * @return the value stored at index n
-         */
-        double &operator[](size_t n);
-
-        /**
-         * at 
-         * @param n the index of the value to be accessed
-         * @throws range_errorwhen n is out of bounds
-         */
-        double &at(size_t n);
-
-        /**
-         * front
-         * @returns reference to the first element in the vector
-         */
-        double &front();
-
-        /**
-         * back
-         * @returns reference to the last element in the vector
-         */
-        double &back();
-
-        /**
-         * data
-         * @returns pointer to the data array
-         */
-        double *data();
-        //where do these go
-        /**
-         * begin
-         * @returns a double_vector::iterator() that looks at the first value in the data array
-         */
-        iterator begin();
-
-        /**
-         * end
-         * @returns a double_vector::iterator() that looks at the position one index past the last value in the data array
-         */
-        iterator end();
-
-        // MODIFIERS
-        /**
-         * push_back
-         * @param val the element to be added to the end of the vector
-         */
-        void push_back(const double &val);
-
-        /**
-         * pop_back
-         * @brief removes the last element in the vector reducing size by one
-         */
-        void pop_back();
-
-        /**
-         * swap
-         * @param other the contents to swap data vector with
-         */
-        void swap(double_vector &other);
-
-        /**
-         * clear
-         * @brief empties the array of all values and sets the size to 0
-         */
-        void clear();
-
-        // ITERATOR SUBCLASS
-        class iterator
-        {
-            double *ptr;
-
-        public:
-            /**
-             * pre-increment operator
-             * @brief moves the iterator forward and returns 
-             */
-            iterator &operator++();
-
-            /**
-             * post-increment operator
-             * @brief moves the iterator forward one step but returns iterator before moving forward
-             */
-            iterator operator++(int);
-
-            /**
-             * pre-decrement operator
-             * @brief moves the iterator backwards one step and returns
-             */
-            iterator &operator--();
-
-            /**
-             * post-decrement operator
-             * @brief moves the iterator backwards one step but returns iterator before moving backward
-             */
-            iterator operator--(int);
-
-            /**
-             * de-reference operator
-             * @returns the location of the data the iterator is looking at
-             */
-            double &operator*();
-
-            /**
-             * equality operator
-             * @param iterator to compare equality (same position in vector, NOT value)
-             * @returns true if they are at same position in vector, false otherwise
-             */
-            bool operator==(const iterator &) const;
-
-            /**
-             * in-equality operator
-             * @param to compare in-equality (not same position in vector)
-             * @returns trues if they are not in the same index or memory location
-             */
-            bool operator!=(const iterator &) const;
-        };
+        bool operator!=(const iterator &) const;
     };
-}
+
+    // where do these go
+    /**
+     * begin
+     * @returns a double_vector::iterator() that looks at the first value in the data array
+     */
+    iterator begin() const;
+
+    /**
+     * end
+     * @returns a double_vector::iterator() that looks at the position one index past the last value in the data array
+     */
+    iterator end() const;
+    
+};
 
 #endif
